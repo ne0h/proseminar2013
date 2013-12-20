4bb2a9dd6a9c1913359a6b4669096508eb86727c
Simon Herter <simon.herter@student.kit.edu>
2013-12-20 23:28:36 +0100

    Initial commit of Java Server, using maven with jetty servlet container. Use maven goal jetty:run and contact server on localhost:8080

---
+++ server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/HelloWorld.java
@@ -0,0 +1,20 @@
+package edu.kit.tm.cm.sc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = -4997896777096757136L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println("<h1>Hello World!</h1>");
+	}
+}
