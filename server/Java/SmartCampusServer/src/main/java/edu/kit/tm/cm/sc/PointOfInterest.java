341ec48519e0bbb37fe05f5bfddeb9a33dab4843
Simon Herter <simon.herter@student.kit.edu>
2014-01-07 15:08:20 +0100

    Changed servlet Container to tomcat7, use maven goal tomcat7:run now!

--- server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterest.java
+++ server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterest.java
@@ -1,7 +1,6 @@
 package edu.kit.tm.cm.sc;
 
 import java.net.URL;
-import java.util.Date;
 import java.util.List;
 
 public class PointOfInterest {
@@ -10,118 +9,95 @@ public class PointOfInterest {
 	private String name;
 	private String building;
 
-	private String street;
-	private int zipCode;
-	private String city;
-	private String country;
+	private Address address;
 
-	private double latitude;
-	private double longitude;
+	private Geolocation geolocation;
 
 	private List<String> tags;
 	private String description;
 	private URL audio;
-	
-	private Date buildDate;
-	private int seats;
-	
-	private List<String> special;
+
+	private Facts facts;
+
 	private URL website;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getBuilding() {
 		return building;
 	}
+
 	public void setBuilding(String building) {
 		this.building = building;
 	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public int getZipCode() {
-		return zipCode;
-	}
-	public void setZipCode(int zipCode) {
-		this.zipCode = zipCode;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public double getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-	public double getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
+
 	public List<String> getTags() {
 		return tags;
 	}
+
 	public void setTags(List<String> tags) {
 		this.tags = tags;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public URL getAudio() {
 		return audio;
 	}
+
 	public void setAudio(URL audio) {
 		this.audio = audio;
 	}
-	public Date getBuildDate() {
-		return buildDate;
-	}
-	public void setBuildDate(Date buildDate) {
-		this.buildDate = buildDate;
-	}
-	public int getSeats() {
-		return seats;
-	}
-	public void setSeats(int seats) {
-		this.seats = seats;
-	}
-	public List<String> getSpecial() {
-		return special;
-	}
-	public void setSpecial(List<String> special) {
-		this.special = special;
-	}
+
 	public URL getWebsite() {
 		return website;
 	}
+
 	public void setWebsite(URL website) {
 		this.website = website;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Geolocation getGeolocation() {
+		return geolocation;
+	}
+
+	public void setGeolocation(Geolocation geolocation) {
+		this.geolocation = geolocation;
+	}
+
+	public Facts getFacts() {
+		return facts;
+	}
+
+	public void setFacts(Facts facts) {
+		this.facts = facts;
 	}
 }
--- server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterestRessource.java
+++ server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterestRessource.java
@@ -1,9 +1,6 @@
 package edu.kit.tm.cm.sc;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -22,68 +19,65 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 
 @Path("/pois")
 public class PointOfInterestRessource {
-	
+
 	private Map<Integer, PointOfInterest> pois = new ConcurrentHashMap<Integer, PointOfInterest>();
 	private AtomicInteger id = new AtomicInteger();
-	
+
+	@SuppressWarnings("deprecation")
 	public PointOfInterestRessource() throws MalformedURLException {
 		PointOfInterest poi = new PointOfInterest();
 		poi.setId(1534);
 		poi.setName("Audimax");
 		poi.setBuilding("30.95");
-		poi.setStreet("Straße am Forum 1");
-		poi.setZipCode(76131);
-		poi.setCity("Karlsruhe");
-		poi.setCountry("Deutschland");
-		poi.setLatitude(49.0134868);
-		poi.setLongitude(8.4162783);
-		poi.setTags(new LinkedList<String>(Arrays.asList("Hörsaal", "Veranstaltungen")));
+		poi.setAddress(new Address("Straße am Forum 1", "76131", "Karlsruhe",
+				"Deutschland"));
+		poi.setGeolocation(new Geolocation(49.0134868, 8.4162783));
+		poi.setTags(new LinkedList<String>(Arrays.asList("Hörsaal",
+				"Veranstaltungen")));
 		poi.setDescription("Der größte Hörsaal des KIT […]");
 		poi.setAudio(new URL("http://www.prototyp.kit.edu/audio/1534.wav"));
-		poi.setBuildDate(new Date(2002, 2, 17));
-		poi.setSeats(734);
-		poi.setSpecial(new LinkedList<String>(Arrays.asList("Getränke- und Snackautomat")));
-		poi.setWebsite(new URL("http://www.google.com"));
+		poi.setFacts(new Facts(new Date(2002, 2, 17), 734,
+				new LinkedList<String>(Arrays
+						.asList("Getränke- und Snackautomat"))));
 		pois.put(poi.getId(), poi);
 	}
-	
+
 	@POST
-	@Consumes("application/json")
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createPOI(InputStream is) {
 		PointOfInterest poi = readPOI(is);
 		poi.setId(id.incrementAndGet());
 		pois.put(poi.getId(), poi);
 		return Response.created(URI.create("/pois/" + poi.getId())).build();
 	}
-	
 
 	@GET
 	@Path("{id}")
-	@Produces("application/json")
-	public StreamingOutput getPOI(@PathParam("id") int id) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public PointOfInterest getPOI(@PathParam("id") int id) {
 		final PointOfInterest poi = pois.get(id);
 		if (poi == null) {
 			throw new WebApplicationException(Response.Status.NOT_FOUND);
 		}
-		return new StreamingOutput() {
-			
-			@Override
-			public void write(OutputStream outputstream) throws IOException,
-					WebApplicationException {
-				outputPOI(outputstream, poi);
-			}
-		};
+		// return new StreamingOutput() {
+		//
+		// @Override
+		// public void write(OutputStream outputstream) throws IOException,
+		// WebApplicationException {
+		// outputPOI(outputstream, poi);
+		// }
+		// };
+		return poi;
 	}
-	
 
 	@PUT
 	@Path("{id}")
-	@Consumes("application/json")
+	@Consumes(MediaType.APPLICATION_JSON)
 	public void updatePOI(@PathParam("id") int id, InputStream is) {
 		PointOfInterest update = readPOI(is);
 		PointOfInterest current = pois.get(id);
@@ -93,55 +87,9 @@ public class PointOfInterestRessource {
 		update.setId(id);
 		pois.put(id, update);
 	}
-	
-	
+
 	private PointOfInterest readPOI(InputStream is) {
 		// TODO Parse InputStream
 		return null;
-	}
-
-	protected void outputPOI(OutputStream outputstream, PointOfInterest poi) {
-		PrintStream writer = new PrintStream(outputstream);
-		writer.println("{");
-		writer.println("\"id\": " + poi.getId() + ",");
-		writer.println("\"name\": " + "\"" + poi.getName() + "\"" + ",");
-		writer.println("\"building\": " + "\"" + poi.getBuilding() + "\"" + ",");
-		writer.println("\"address\": {");
-			writer.println("\"street\": " + "\"" + poi.getStreet() + "\"" + ",");
-			writer.println("\"zipCode\": " + "\"" + poi.getZipCode() + "\"" + ",");
-			writer.println("\"city\": " + "\""+ poi.getCity() + "\"" + ",");
-			writer.println("\"country\": " + "\"" + poi.getCountry() + "\"");
-		writer.println("},");
-		writer.println("\"geolocation\": {");
-			writer.println("\"latitude\": " + poi.getLatitude() + ",");
-			writer.println("\"longitude\": " + poi.getLongitude() + ",");
-		writer.println("},");
-		writer.print("\"tags\": [");
-		int size = poi.getTags().size();
-			for (int i = 0; i < size; i++) {
-				writer.print("\"" + poi.getTags().get(i) + "\"");
-				if (i < size - 1) {
-					writer.print(", ");
-				}
-			}
-		writer.println("],");
-		writer.println("\"description\": " + "\"" + poi.getDescription() + "\"" + ",");
-		writer.println("\"audio\": " + "\"" + poi.getAudio().toString() + "\"" + ",");
-		writer.println("\"facts\": {");
-			writer.println("\"buildDate\": " + "\"" + poi.getBuildDate().toString() + "\"" + ",");
-			writer.println("\"seats\": " + poi.getSeats() + ",");
-			writer.print("\"tags\": [");
-			size = poi.getSpecial().size();
-				for (int i = 0; i < size; i++) {
-					writer.print("\"" + poi.getSpecial().get(i) + "\"");
-					if (i < size - 1) {
-						writer.print(", ");
-					}
-				}
-			writer.println("]");
-		writer.println("},");
-		writer.println("\"website\": " + "\"" + poi.getWebsite().toString() + "\"");
-		writer.println("}");
-		
 	}
 }
