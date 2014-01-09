8e136ff570bd91a957c05c31cb9aadb8c57f1ef0
Simon Herter <simon.herter@student.kit.edu>
2014-01-10 00:27:31 +0100

    Implemented DELETE und PUT on /pois/xxxx

--- server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterestRessource.java
+++ server/Java/SmartCampusServer/src/main/java/edu/kit/tm/cm/sc/PointOfInterestRessource.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -22,25 +23,28 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Path("/pois")
 public class PointOfInterestRessource {
 
-	private static Map<Integer, PointOfInterest> pois = new ConcurrentHashMap<Integer, PointOfInterest>();
-	private static AtomicInteger id = new AtomicInteger();
+	private static final Map<Integer, PointOfInterest> pois;
+	private static final AtomicInteger id;
 
-	public PointOfInterestRessource() throws JsonParseException,
-			JsonMappingException, IOException {
+	static {
+		pois = new ConcurrentHashMap<Integer, PointOfInterest>();
+		id = new AtomicInteger();
 		ObjectMapper m = new ObjectMapper();
-		PointOfInterest[] defaultPois = m.readValue(new File(
-				"src/main/resources/pois.json"), PointOfInterest[].class);
+		PointOfInterest[] defaultPois;
+		try {
+			defaultPois = m.readValue(new File("src/main/resources/pois.json"),
+					PointOfInterest[].class);
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
 		for (PointOfInterest poi : defaultPois) {
 			pois.put(poi.getId(), poi);
 		}
-
 	}
 
 	@GET
@@ -50,9 +54,7 @@ public class PointOfInterestRessource {
 		if (search == null || search.equals("")) {
 			return pois.values();
 		}
-
 		List<PointOfInterest> filterdPOIs = new LinkedList<PointOfInterest>();
-
 		for (PointOfInterest poi : pois.values()) {
 			if (poi.getName().toLowerCase().contains(search.toLowerCase())) {
 				filterdPOIs.add(poi);
@@ -75,21 +77,35 @@ public class PointOfInterestRessource {
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public PointOfInterest getPOI(@PathParam("id") int id) {
-		final PointOfInterest poi = pois.get(id);
-		if (poi == null) {
+		if (!pois.containsKey(id)) {
 			throw new WebApplicationException(Response.Status.NOT_FOUND);
 		}
-		return poi;
+		return pois.get(id);
 	}
 
 	@PUT
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void updatePOI(PointOfInterest update) {
-		if (!pois.containsKey(update.getId())) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public PointOfInterest updatePOI(@PathParam("id") int id, PointOfInterest poi) {
+		if (!pois.containsKey(id)) {
 			throw new WebApplicationException(Response.Status.NOT_FOUND);
 		}
-		pois.put(update.getId(), update);
+		if (poi == null || (poi.getId() != 0 && id != poi.getId())) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		poi.setId(id);
+		pois.put(id, poi);
+		return poi;
+	}
+
+	@DELETE
+	@Path("{id}")
+	public Response deletePOI(@PathParam("id") int id) {
+		if (pois.remove(id) == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		return Response.noContent().build();
 	}
 
 }
